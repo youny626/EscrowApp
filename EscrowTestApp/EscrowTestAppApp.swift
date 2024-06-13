97d645335791d2f414eb81d4302fadbb706afdc6
youny626 <zhiru@uchicago.edu>
2024-06-12 21:19:32 -0500

    Initial Commit

---
+++ EscrowTestApp/EscrowTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscrowTestAppApp.swift
+//  EscrowTestApp
+//
+//  Created by Zhiru Zhu on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EscrowTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
